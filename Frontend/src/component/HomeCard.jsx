26eaa7d14d7a36cd3ec8673afc5a45a3b91c3468
Swarnim2410 <swarnimagupta2124@gmail.com>
2024-03-30 12:42:26 +0530

    display-changes

--- Frontend/src/component/HomeCard.jsx
+++ Frontend/src/component/HomeCard.jsx
@@ -4,7 +4,7 @@ const HomeCard = ({ name, image, price, category, description }) => {
   return (
     <Link to={`/${category}`}>
       <div className="py-4 md:w-72 lg:w-64 px-4 bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700 shadow-md">
-        <img className="h-20 w-20 mx-auto" alt="" src={image} />
+        <img className="h-20 w-20 mx-auto bg-black" alt="" src={image} />
         <div className="">
           <h5 className="capitalize text-center mt-2 mb-1 text-sm font-bold tracking-tight text-gray-900 dark:text-white overflow-hidden whitespace-nowrap">
             {name}
